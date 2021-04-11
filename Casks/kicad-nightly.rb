136cb4d4ff9fc8b72053249b1be3a9361c9c17b2
Indexyz <jiduye@gmail.com>
2021-04-11 16:07:52 +0000

    feat: upgrade kicad to 20210411-061509-e4f9075160

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -1,6 +1,6 @@
 cask "kicad-nightly" do
-  version "20210410-190620-90dd576e78"
-  sha256 "fef58b97114f5477fe19c43ea6d261ccec239007ed170ad4165a1f815630c503"
+  version "20210411-061509-e4f9075160"
+  sha256 "c12afccdade67e8ed9c444ff89f1580e92508418636d2026b0e659b6695836ef"
 
   url "https://kicad-downloads.s3.cern.ch/osx/nightly/kicad-unified-#{version}-10_14.dmg",
       verified: "kicad-downloads.s3.cern.ch/"
