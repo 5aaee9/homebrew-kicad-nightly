ada739025a242e4e00042c1509d26f20c192b887
Indexyz <jiduye@gmail.com>
2023-02-03 20:13:58 +0000

    feat: upgrade kicad to universal-20230203-0236-9b9ebebb2d

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -1,5 +1,5 @@
 cask "kicad-nightly" do
-  version "universal-20230203-0236"
+  version "universal-20230203-0236-9b9ebebb2d"
   sha256 "304ff3b010932170dd346af6424d570e459fb1c067639404f6604b458bac5e00"
 
   url "https://kicad-downloads.s3.cern.ch/osx/nightly/kicad-unified-#{version}.dmg",
@@ -9,21 +9,20 @@ cask "kicad-nightly" do
   desc "Electronics design automation suite"
   homepage "https://kicad.org/"
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
-  app "KiCad/KiCad.app",              target: "KiCad/KiCad.app"
-  app "KiCad/Bitmap2Component.app",   target: "KiCad/Bitmap2Component.app"
-  app "KiCad/Eeschema.app",           target: "KiCad/Eeschema.app"
-  app "KiCad/GerbView.app",           target: "KiCad/GerbView.app"
-  app "KiCad/PCB Calculator.app",     target: "KiCad/PCB Calculator.app"
-  app "KiCad/Pcbnew.app",             target: "KiCad/Pcbnew.app"
-  app "KiCad/Page Layout Editor.app", target: "KiCad/Page Layout Editor.app"
-  artifact "kicad",          target: "/Library/Application Support/kicad/"
+  suite "KiCad"
+  artifact "demos", target: "/Library/Application Support/kicad/demos"
 
-  uninstall rmdir: [
+  zap trash: [
     "/Library/Application Support/kicad",
-    "#{appdir}/KiCad",
+    "~/Library/Application Support/kicad",
+    "~/Library/Preferences/kicad",
+    "~/Library/Preferences/org.kicad-pcb.*",
+    "~/Library/Saved Application State/org.kicad-pcb.bitmap2component.savedState",
+    "~/Library/Saved Application State/org.kicad-pcb.eeschema.savedState",
+    "~/Library/Saved Application State/org.kicad-pcb.kicad.savedState",
+    "~/Library/Saved Application State/org.kicad-pcb.pcb_calculator.savedState",
+    "~/Library/Saved Application State/org.kicad-pcb.pl_editor.savedState",
   ]
-
-  zap trash: "~/Library/Preferences/kicad"
 end
