c7fa4b84ceef2f69e913de8533f45d191e14c6ed
Indexyz <jiduye@gmail.com>
2023-10-24 16:01:24 +0000

    feat: upgrade kicad to universal-20231024-1011-00904e8

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -1,6 +1,6 @@
 cask "kicad-nightly" do
-  version "universal-20231023-1012-97901c9"
-  sha256 "3d102f137f35a67d76eb2254801fc038b531627ffdf4636fff893cae6f3f613b"
+  version "universal-20231024-1011-00904e8"
+  sha256 "81c8fdaa057536acffd85f8b388d42197f03df43d4dc616efd2373975ed50094"
 
   url "https://kicad-downloads.s3.cern.ch/osx/nightly/kicad-unified-#{version}.dmg",
       verified: "kicad-downloads.s3.cern.ch/"
