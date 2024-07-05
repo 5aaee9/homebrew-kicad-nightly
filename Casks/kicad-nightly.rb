ee17cb8ca76ed0e6a0b8acb5a6269a9d7b771212
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2024-07-05 17:01:03 -0500

    update url to use the official loadbalanced link

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -2,9 +2,9 @@ cask "kicad-nightly" do
   version "universal-20240705-1023-a1b4e09cec"
   sha256 "b84e534ec34b3a7548f115d0a11b2b1ab7399cd5b7741b1357fc3c582d278552"
 
-  url "https://kicad-downloads.s3.cern.ch/osx/nightly/kicad-unified-#{version}.dmg",
-      verified: "kicad-downloads.s3.cern.ch/"
-  appcast "https://kicad-downloads.s3.cern.ch/?delimiter=/&prefix=osx/nightly/"
+  url "https://downloads.kicad.org/kicad/macos/explore/nightlies/download/kicad-unified-#{version}.dmg",
+      verified: "downloads.kicad.org/"
+  appcast "https://downloads.kicad.org/?delimiter=/&prefix=osx/nightly/"
   name "KiCad"
   desc "Electronics design automation suite"
   homepage "https://kicad.org/"
